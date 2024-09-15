1abb3f8baf8be726266fb569d06f73679a70dd56
Saurabhmishra214 <oosaurabh6@gmail.com>
2024-09-15 11:19:24 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+echo"welcome to associative array ";
+$favcol=array('saurabh '=>'black','vicky'=>'red','abhishek'=>'broun');
+
+//yah loop tb tk chalta rahe ga jb tak array ki sabhi value khata na ho jaye 
+foreach($favcol as $key => $value)
+{
+    echo"<br> favorite colo of $key is $value";
+} 
+*/
+echo"multi dimensional array"
+//multi dimensional array ka matlab hota hai ki hm jab array ke andar array ko dal dete hai to us ko multi pal dimensional array kahte hai 
+
+//vardump function ka use data type ko pata karne ke liye use kiya jata hai 
+?>
